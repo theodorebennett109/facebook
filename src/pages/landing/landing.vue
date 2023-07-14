0fa7cae36b411226cbb4c236cdcf9a7c352333ad
theodorebennett109 <theodorebennett3@gmail.com>
2023-07-14 00:46:49 -0500

    landing and home page created

---
+++ src/pages/landing/landing.vue
@@ -0,0 +1,7 @@
+<style scoped lang="scss" src="./landing.scss"></style>
+<script lang="ts" src="./landing.ts"></script>
+<template>
+  <div class="[ landing ]">
+    <h1>Sample Page</h1>
+  </div>
+</template>
